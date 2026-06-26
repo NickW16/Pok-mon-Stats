31d8cb61f681197654ff27fc8afde6f5edbde100
NickW16 <171855220+NickW16@users.noreply.github.com>
2026-06-26 12:03:14 -0300

    huge images insertion, fix database to provide correct links and fix views to render correct images

--- src/db/seedDatabase.js
+++ src/db/seedDatabase.js
@@ -1,4 +1,7 @@
+require('dotenv').config({ path: '../../.env' });
+console.log('DATABASE_URL:', process.env.DATABASE_URL); // debug to connect db
 const { pool } = require ('./pool');
+const fs = require('fs');
 const axios = require('axios');
 
 const POKEAPI_BASE = 'https://pokeapi.co/api/v2';
@@ -16,14 +19,31 @@ async function seedDatabase() {
          const detail = await axios.get(pokemon.url);
          const d = detail.data;
 
+         // gameboy sprites
+         const gameboyImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${d.id}.png`;
+         const gameboyImagePath = `../public/images/pokemon-images/${d.id}.png`;
+
+         const gameboyImageResponse = await axios.get(gameboyImageUrl, { responseType: 'stream' });
+
+         gameboyImageResponse.data.pipe(fs.createWriteStream(gameboyImagePath));
+
+         // official images
+         const officialImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${d.id}.png`;
+         const officialImagePath = `../public/images/pokemon-images/official-${d.id}.png`;
+
+         const officialImageResponse = await axios.get(officialImageUrl, { responseType: 'stream' });
+         officialImageResponse.data.pipe(fs.createWriteStream(officialImagePath));
+
          await pool.query(
-            `INSERT INTO pokemon (name, pokedex_id, types)
-            VALUES ($1, $2, $3)
+            `INSERT INTO pokemon (name, pokedex_id, types, official_image_url, gameboy_image_url)
+            VALUES ($1, $2, $3, $4, $5)
             ON CONFLICT (pokedex_id) DO NOTHING`,
             [
                d.name,
                d.id,
                d.types.map(t => t.type.name), // convert to array
+               `/images/pokemon-images/official-${d.id}.png`,
+               `/images/pokemon-images/${d.id}.png`
             ]
          );
 
